17f5797b9c1c2440e7f9ae6762c0daa32aa95562
Dotton <dotton95@gmail.com>
2022-01-31 23:25:15 +0900

    1/31

--- app/src/main/java/my/dotton/fuddy_app/Fragment/HomeFragment.kt
+++ app/src/main/java/my/dotton/fuddy_app/Fragment/HomeFragment.kt
@@ -27,6 +27,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home){
     }
     //현재 위도 경도의 주소 받아오기
     private fun getCurrentAddress(lat:Double,lon:Double):String{
+
         var mResult:List<Address> = Geocoder(requireContext(), Locale.KOREAN).getFromLocation(lat,lon,1)
 
         var admin = if(mResult.get(0).adminArea!=null) mResult.get(0).adminArea else mResult.get(0).subAdminArea
