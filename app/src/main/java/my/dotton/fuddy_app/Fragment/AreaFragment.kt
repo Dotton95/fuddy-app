c3a50f98ba6f0c028dc46f34c77b82352ee7d065
Dotton <dotton95@gmail.com>
2022-03-03 22:05:16 +0900

    3/3

--- app/src/main/java/my/dotton/fuddy_app/Fragment/AreaFragment.kt
+++ app/src/main/java/my/dotton/fuddy_app/Fragment/AreaFragment.kt
@@ -9,6 +9,29 @@ import my.dotton.fuddy_app.R
 import my.dotton.fuddy_app.databinding.FragmentAreaBinding
 
 class AreaFragment : BaseFragment<FragmentAreaBinding>(R.layout.fragment_area) {
+
+//    type json
+//
+//    prmisnZoneNm 허가구역명
+//    ctprvnNm  시도명
+//    signguNm 시군구명
+//    lnmadr 소재지지번주소
+//    latitude 위도
+//    longitude 경도
+//    vhcleCo 푸드트럭운영대수
+//    primsnZoneRntfee 허가구역 사용료
+//    beginDate 허가구역운영시작일자
+//    endDate 허가구역운영종료일자
+//    rstde 허가구역휴무일
+//    weekdayOperOpenHhmm 허가구역평일운영시작시각
+//    weekdayOperColseHhmm 허가구역평일운영종료시각
+//    wkendOperOpenHhmm 허가구역주말운영시작시각
+//    wkendOperColseHhmm 허가구역주말운영종료시각
+//    lmttPrdlst 판매제한품목
+//    institutionNm 관리기관명
+//    phoneNumber 관리기관 전화번호
+//    referenceDate 데이터 기준일자
+
     override fun initView() {
         super.initView()
         binding.apply {
